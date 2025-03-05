7455fb607f98b1ed7289fd5df2efce9297ed266d
vatsal <mehtavatsal16@gmail.com>
2025-03-05 15:46:25 +0530

    next

---
+++ loop pattern/p4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int i,j,m=i;
+	for (i=1; i<=5; i++)
+	{
+		for(j=1; j<=5; j++)
+		{
+			printf("\t %d",m);
+			m++;
+		}
+			printf("\n");		
+	}
+	
+}
---
+++ loop pattern/p5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+	int i;
+	for(i=1; i<=4; i++)
+	{
+		for(j=1;j<i*2;j++)
+		{
+			
+		}
+	}
+}
